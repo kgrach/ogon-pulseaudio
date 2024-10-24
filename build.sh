c01a2616e9aa8447f526a67dbaa3161d361488ec
Kirill Grachev <pllayer@mail.ru>
2024-10-24 22:45:28 +0700

    Upd scripts

---
+++ build.sh
@@ -0,0 +1,6 @@
+export GIT_DESCRIBE_FOR_BUILD=2.25.1
+export NOCONFIGURE=YES
+./bootstrap.sh
+export PKG_CONFIG_PATH=/opt/ogon/lib/pkgconfig/:/opt/ogon/share/pkgconfig:/opt/ogon/lib/x86_64-linux-gnu/pkgconfig/:/usr/lib/x86_64-linux-gnu/pkgconfig/
+./configure -disable-oss-output --enable-oss-wrapper --disable-alsa --disable-jack --disable-xen --disable-tests --disable-udev --enable-ogon --prefix=/opt/ogon --disable-glib2 --disable-avahi --disable-ipv6 --disable-openssl --enable-x11 --disable-systemd-journal --disable-systemd-daemon LDFLAGS="-Wl,-rpath=/opt/ogon/lib:/opt/ogon/lib/x86_64-linux-gnu/"
+make -j $(grep -c '^processor' /proc/cpuinfo) install
---
+++ install_depends_ubuntu22.sh
@@ -0,0 +1,2 @@
+sudo apt install libsm-dev libxtst-dev libx11-xcb-dev intltool autopoint libltdl-dev \
+		 libcap-dev libsm-dev libjson-c-dev libsndfile1-dev
