deef903d7cf1864a90f6e4bfb80e45019218ca6b
Kirill Grachev <pllayer@mail.ru>
2024-10-31 08:11:40 +0700

    Upd install script 4 redos

---
+++ install_depends.sh
@@ -0,0 +1,2 @@
+sudo dnf install -y libSM-devel libXtst-devel intltool libtool-ltdl-devel \
+                    libcap-devel cjson-devel json-c-devel libsndfile-devel
--- install_depends_ubuntu22.sh
+++
@@ -1,2 +0,0 @@
-sudo apt install -y libsm-dev libxtst-dev libx11-xcb-dev intltool autopoint libltdl-dev \
-		    libcap-dev libsm-dev libjson-c-dev libsndfile1-dev
